666356fc128dd86ff65449339afd3cf9d93b602d
reishelly <nellelviralen@gmail.com>
2025-02-19 20:23:50 +0700

    main

--- luas.py
+++ luas.py
@@ -1,1 +1,10 @@
-print("HelloWorld!")
+#ELLEN ELVIRA 535240023
+# 1. Fungsi Python Luas dan Keliling Persegi Panjang
+panjang= float (input("Masukkan panjang= "))
+lebar= float(input("Masukkan lebar= "))
+
+luas= panjang *lebar
+keliling= 2*(panjang+lebar)
+print("Keliling persegi panjang= ", keliling)
+print("Luas persegi panjang= ", luas)
+
